c10d22be80748610681ea931d754559d76ffce8a
Nikhil-Arjun <nikhilarjun97@gmail.com>
2024-03-21 23:24:23 +0530

    Emi section

---
+++ src/components/Emi.jsx
@@ -0,0 +1,17 @@
+const Emi = () => {
+  return (
+    <div className="h-12 bg-gray-950 text-white px-4 flex items-center justify-center text-center text-lg md:text-base cursor-pointer">
+      <p className="font-helvetica">
+        Save ₹6000.00 instantly on iPhone 15 Pro with HDFC Bank Credit Cards.
+        Plus No Cost EMI from most leading banks.{" "}
+        <button className="text-blue underline">
+          <a href="https://www.apple.com/in/shop/buy-iphone/iphone-15-pro">
+            Buy
+          </a>
+        </button>
+      </p>
+    </div>
+  );
+};
+
+export default Emi;
