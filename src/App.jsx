ca3cb5c49d764121ae65611e2880d0dc1dd93009
Nikhil-Arjun <nikhilarjun97@gmail.com>
2024-03-21 20:02:27 +0530

    Navbar , Hero , highlights section

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import "./index.css";
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
+};
+
+export default App;
