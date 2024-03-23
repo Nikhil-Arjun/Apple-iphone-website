51a4e8e35710dd0a8b1cd5243ad25cf4efe33760
Nikhil-Arjun <nikhilarjun97@gmail.com>
2024-03-23 11:22:28 +0530

    update

--- src/components/Emi.jsx
+++ src/components/Emi.jsx
@@ -12,7 +12,7 @@ const Emi = () => {
   return (
     <div
       id="emi"
-      className="h-12 bg-gray-950 text-white px-4 flex items-center justify-center text-center text-lg md:text-base cursor-pointer sm:hidden">
+      className="h-12 bg-gray-950 text-white px-4 flex items-center justify-center text-center text-lg md:text-base cursor-pointer">
       <p className="font-sans">
         Save ₹6000.00 instantly on iPhone 15 Pro with HDFC Bank Credit Cards.
         Plus No Cost EMI from most leading banks.{" "}
