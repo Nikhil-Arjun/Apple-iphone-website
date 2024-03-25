e60b150d0be75c057d23d5c2d409cf4058e61e12
Nikhil-Arjun <nikhilarjun97@gmail.com>
2024-03-25 14:04:53 +0530

    Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,27 @@
 import { Html } from "@react-three/drei";
+import { useEffect, useState } from "react";
+import { ClimbingBoxLoader } from "react-spinners";
 
 const Loader = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
   return (
     <Html>
       <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-        <div className="w-[10vw] h-[10vw] rounded-full">Loading...</div>
+        <div className="w-[10vw] h-[10vw] rounded-full">
+          {loading ? (
+            <ClimbingBoxLoader color={"FEFDED"} loading={loading} size={100} />
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </Html>
   );
